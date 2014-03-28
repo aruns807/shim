e43596e8f70b92bacaa7a494fc788510afc61a47
Thiago Kenji Okada <thiago.mast3r@gmail.com>
2014-03-28 18:13:38 -0300

    Use argparse instead of optparse to parse cli arguments
    Optparse is deprecated since Python 2.7 (see
    http://docs.python.org/2/library/optparse.html). Argparse have a
    similar usage but is much more robust (i.e., it don't allow positional
    option without declaring it first). The only problem is that Argparse
    was released on Python 2.7 (and Python 3.2 if I am not mistaken),
    but Python 2.7 is already 4 years old so who cares ;) .
    
    I made the options more robust too, making every argument optional and
    exclusive (since this is how the code was, using if/else if/else for
    each option), so the user is not confused (i.e. if he tries to use more
    than a option the parser gives the proper error instead of allowing
    shim to run but only executing the option with higher priority).
    
    Finally, we inform the user how to use the program instead of just
    printing a "failure!" message if he runs shim without any option (like
    someone posted on Reddit).

--- src/shim.py
+++ src/shim.py
@@ -2,10 +2,18 @@
 from Tkinter import Tk, Image
 from Frontend import text_canvas
 from Backend import user_input
-from optparse import OptionParser
-from os import path
+from argparse import ArgumentParser
+import loader, metadata, os.path, sys
 
-parser = OptionParser()
+def opt_init():
+    parser = ArgumentParser(description='A vim inspired text editor for all')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument(dest='filename', action='store', metavar='FILE', nargs='?')
+    group.add_argument('-m', '--load-meta-data', dest='load_meta_data',
+                      action='store_true', default=False, help='Run meta data loader')
+    group.add_argument('-l', '--load-plugins', dest='load_plugins', metavar='PATH',
+                      action='store', default=None, help='Run plugin loader with directory name')
+    return parser
 
 def run_text_editor(filename):
     root = Tk()
@@ -22,9 +30,23 @@ def run_text_editor(filename):
 
 
 if __name__ == '__main__':
-    (options, args) = parser.parse_args()
+    parser = opt_init()
+    args = parser.parse_args()
 
-    try:
-        run_text_editor(args[0])
-    except IndexError:
-        print 'failure! Did not specify file to edit'
+    if args.load_plugins != None:
+        print 'LOADING PLUGIN CODE'
+        loader.load_plugin(args.load_plugins)
+    
+    elif args.load_meta_data:
+        print 'CREATING META DATA FILES'
+        metadata.create_metadata_files()
+    else:
+        if not os.path.isfile('.shimdata'):
+            print 'META DATA FILE DOES NOT EXIST YET'
+            print 'GENERATING META DATA FILES'
+            metadata.create_metadata_files()
+
+        if args.filename:
+            run_text_editor(args.filename)
+        else:
+            parser.print_usage()
