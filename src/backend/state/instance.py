dd87a94cd4e722ad7b7dc4deb5fd49567efd6cea
Sebastian Wong <wngsebastian@gmail.com>
2014-04-15 21:49:46 -0500

    fixing naming of backend private variables

--- src/backend/state/instance.py
+++ src/backend/state/instance.py
@@ -9,22 +9,22 @@ from backend.state.syntaxtokens import syntax_parser
 class instance():
 
     def __init__(self, filename):
-        self.filename = filename
-        self.parser = syntax_parser.syntax_parser(filename)
+        self._filename = filename
+        self._parser = syntax_parser.syntax_parser(filename)
         if os.path.exists(filename):
-            self.lines = [
+            self._lines = [
                 line for line in open(filename, 'r')
             ]
-            self.line_tokens = [
-                self.parser.parse_string(line) for line in open(filename, 'r')
+            self._line_tokens = [
+                self._parser.parse_string(line) for line in open(filename, 'r')
             ]
         else:
-            self.lines = ['']
-            self.line_tokens = ['']
+            self._lines = ['']
+            self._line_tokens = ['']
 
-        self.cursor_x, self.cursor_y, self.curr_top = 0, 0, 0
-        self.visual_x, self.visual_y, self.visual_curr_top = 0, 0, 0
-        self.undo_buffer, self.undo_index = [], -1
+        self._cursor_x, self._cursor_y, self._curr_top = 0, 0, 0
+        self._visual_x, self._visual_y, self._visual_curr_top = 0, 0, 0
+        self._undo_buffer, self._undo_index = [], -1
 
     def check_repeat_additions(self, diff, last_state):
         return (
@@ -49,12 +49,12 @@ class instance():
         Only add to undo buffer is move is not to be coalesced into
         previous undo block
         """
-        if self.undo_index == len(self.undo_buffer) - 1:
+        if self._undo_index == len(self._undo_buffer) - 1:
             try:
-                last_state = self.undo_buffer[self.undo_index]
+                last_state = self._undo_buffer[self._undo_index]
             except IndexError:
-                self.undo_buffer.append(diff)
-                self.undo_index += 1
+                self._undo_buffer.append(diff)
+                self._undo_index += 1
                 return
 
             # repeat addtions to the same line
@@ -72,39 +72,39 @@ class instance():
                 last_state[2]['new']['line_token'] = \
                     diff[2]['new']['line_token']
             else:
-                self.undo_buffer.append(diff)
-                self.undo_index += 1
-                if len(self.undo_buffer) > 100:
-                    self.undo_buffer.pop(0)
+                self._undo_buffer.append(diff)
+                self._undo_index += 1
+                if len(self._undo_buffer) > 100:
+                    self._undo_buffer.pop(0)
         else:
-            self.undo_buffer = (
-                self.undo_buffer[:self.undo_index] + [diff], [diff]
-            )[self.undo_index == -1]
-            self.undo_index += 1
+            self._undo_buffer = (
+                self._undo_buffer[:self._undo_index] + [diff], [diff]
+            )[self._undo_index == -1]
+            self._undo_index += 1
 
     def undo_line_modification(self, diff):
-        self.lines[diff[1]] = diff[2]['old']['line'][0]
-        self.line_tokens[diff[1]] = diff[2]['old']['line_token'][0]
+        self._lines[diff[1]] = diff[2]['old']['line'][0]
+        self._line_tokens[diff[1]] = diff[2]['old']['line_token'][0]
 
     def undo_line_addition(self, diff):
         for i in range(diff[2]['count']):
-            self.lines.pop(diff[1])
-            self.line_tokens.pop(diff[1])
+            self._lines.pop(diff[1])
+            self._line_tokens.pop(diff[1])
 
     def undo_line_removal(self, diff):
         for i in range(len(diff[2]['lines'])):
-            self.lines.insert(i + diff[1], diff[2]['lines'][i])
-            self.line_tokens.insert(i + diff[1], diff[2]['line_tokens'][i])
+            self._lines.insert(i + diff[1], diff[2]['lines'][i])
+            self._line_tokens.insert(i + diff[1], diff[2]['line_tokens'][i])
 
     def undo_state(self):
         """
         Only undo if there is an action to undo
         set cursor and curr_top coordinates to saved coordinates
         """
-        if self.undo_index != -1:
-            # diff = self.undo_buffer.pop(-1)
-            diff = self.undo_buffer[self.undo_index]
-            self.undo_index -= 1
+        if self._undo_index != -1:
+            # diff = self._undo_buffer.pop(-1)
+            diff = self._undo_buffer[self._undo_index]
+            self._undo_index -= 1
             if diff[0] == 'm':
                 self.undo_line_modification(diff)
             elif diff[0] == '+':
@@ -117,28 +117,28 @@ class instance():
             self.set_cursor(x, y)
 
     def redo_line_modification(self, diff):
-        self.lines[diff[1]] = diff[2]['new']['line'][0]
-        self.line_tokens[diff[1]] = diff[2]['new']['line_token'][0]
+        self._lines[diff[1]] = diff[2]['new']['line'][0]
+        self._line_tokens[diff[1]] = diff[2]['new']['line_token'][0]
 
     def redo_line_addition(self, diff):
         for i in range(diff[2]['count']):
-            self.lines.insert(diff[1] + i, diff[2]['data']['lines'][i])
-            self.line_tokens.insert(
+            self._lines.insert(diff[1] + i, diff[2]['data']['lines'][i])
+            self._line_tokens.insert(
                 diff[1] + i, diff[2]['data']['line_tokens'][i]
             )
 
     def redo_line_removal(self, diff):
         for i in range(len(diff[2]['lines'])):
-            self.lines.pop(diff[1])
-            self.line_tokens.pop(diff[1])
+            self._lines.pop(diff[1])
+            self._line_tokens.pop(diff[1])
 
     def redo_state(self):
         """
         Only redo if there is an action to redo
         """
-        if self.undo_index < len(self.undo_buffer) - 1:
-            self.undo_index += 1
-            diff = self.undo_buffer[self.undo_index]
+        if self._undo_index < len(self._undo_buffer) - 1:
+            self._undo_index += 1
+            diff = self._undo_buffer[self._undo_index]
             if diff[0] == 'm':
                 self.redo_line_modification(diff)
             if diff[0] == '+':
@@ -147,35 +147,35 @@ class instance():
                 self.redo_line_removal(diff)
 
     def get_line(self, index):
-        return self.lines[index]
+        return self._lines[index]
 
     # line numbers are 0 indexed
     def get_lines(self):
-        return self.lines
+        return self._lines
 
     def get_line_tokens(self):
-        return self.line_tokens
+        return self._line_tokens
 
     def get_cursor(self):
-        return self.cursor_x, self.cursor_y
+        return self._cursor_x, self._cursor_y
 
     def get_curr_top(self):
-        return self.curr_top
+        return self._curr_top
 
     def get_line_height(self):
-        return self.line_height
+        return self._line_height
 
     def get_line_num(self):
-        return len(self.lines)
+        return len(self._lines)
 
     def get_filename(self):
-        return self.filename
+        return self._filename
 
     def get_page_state(self):
-        return self.cursor_x, self.cursor_y, self.curr_top
+        return self._cursor_x, self._cursor_y, self._curr_top
 
     def get_visual_anchors(self):
-        return self.visual_x, self.visual_y, self.visual_curr_top
+        return self._visual_x, self._visual_y, self._visual_curr_top
 
     def add_line(self, index, line):
         """
@@ -183,11 +183,11 @@ class instance():
         redo can be performed
         """
         try:
-            li = self.lines[index]
-            lt = self.line_tokens[index]
+            li = self._lines[index]
+            lt = self._line_tokens[index]
         except IndexError:
             li = ''
-            lt = self.parser.parse_string('')
+            lt = self._parser.parse_string('')
 
         d = {
             'count': 1,
@@ -200,8 +200,8 @@ class instance():
         }
 
         self.add_to_undo_buffer(('+', index, d))
-        self.lines.insert(index, line)
-        self.line_tokens.insert(index, self.parser.parse_string(line))
+        self._lines.insert(index, line)
+        self._line_tokens.insert(index, self._parser.parse_string(line))
 
     def remove_line(self, index):
         """
@@ -209,19 +209,19 @@ class instance():
         redo can be performed
         """
         d = {
-            'lines': [self.lines[index]],
-            'line_tokens': [self.line_tokens[index]],
+            'lines': [self._lines[index]],
+            'line_tokens': [self._line_tokens[index]],
             'state': self.get_page_state(),
         }
         self.add_to_undo_buffer(('-', index, d))
-        self.lines.pop(index)
-        self.line_tokens.pop(index)
+        self._lines.pop(index)
+        self._line_tokens.pop(index)
 
     def set_curr_top(self, num):
-        self.curr_top = num
+        self._curr_top = num
 
     def set_line_height(self, num):
-        self.line_height = num
+        self._line_height = num
 
     def set_line(self, ind, s):
         """
@@ -229,11 +229,11 @@ class instance():
         undo buffer. Pick string depending upon
         undo or redo action
         """
-        parsed = self.parser.parse_string(s)
+        parsed = self._parser.parse_string(s)
         d = {
             'old': {
-                'line': [self.lines[ind]],
-                'line_token': [self.line_tokens[ind]]
+                'line': [self._lines[ind]],
+                'line_token': [self._line_tokens[ind]]
             },
             'new': {
                 'line': [s],
@@ -243,30 +243,30 @@ class instance():
         }
 
         self.add_to_undo_buffer(('m', ind, d))
-        self.lines[ind] = s
-        self.line_tokens[ind] = parsed
+        self._lines[ind] = s
+        self._line_tokens[ind] = parsed
 
     def set_visual_anchor(self, x=None, y=None, curr_top=None):
         """
         Set anchors to cursor and curr_top values if no
         arguments ar passed in
         """
-        self.visual_x = x if x is not None else self.cursor_x
-        self.visual_y = y if y is not None else self.cursor_y
-        self.visual_curr_top = \
-            curr_top if curr_top is not None else self.curr_top
+        self._visual_x = x if x is not None else self._cursor_x
+        self._visual_y = y if y is not None else self._cursor_y
+        self._visual_curr_top = \
+            curr_top if curr_top is not None else self._curr_top
 
     def set_cursor(self, x, y):
         """
         Set cursor while making sure cursor x and cursor y
         never get invalid values
         """
-        self.cursor_x = max(x, 0)
-        if y > self.line_height:
-            self.curr_top += (y - self.line_height)
-            self.cursor_y = self.line_height
+        self._cursor_x = max(x, 0)
+        if y > self._line_height:
+            self._curr_top += (y - self._line_height)
+            self._cursor_y = self._line_height
         elif y < 0:
-            self.curr_top = max(self.curr_top + y, 0)
-            self.cursor_y = 0
+            self._curr_top = max(self._curr_top + y, 0)
+            self._cursor_y = 0
         else:
-            self.cursor_y = y
+            self._cursor_y = y
